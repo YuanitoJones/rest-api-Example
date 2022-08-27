1882a09bfa3222aca20ba2b09c21de841fe7da48
JuanRenteriaAbril <jrenteria@brconsultores.com.mx>
2022-08-26 18:29:20 -0700

    tamaños de imagenes y otros margenes

---
+++ rest_api/build/database.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_mongoose["default"].connect("mongodb://127.0.0.1/apirestdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(function (db) {
+  return console.log('Db is connected');
+})["catch"](function (error) {
+  return console.log('Error loading db');
+});
--- rest_api/build/index.js
+++ rest_api/build/index.js
@@ -1,10 +1,11 @@
 "use strict";
 
-var _express = _interopRequireDefault(require("express"));
+var _app = _interopRequireDefault(require("./app.js"));
+
+require("./database.js");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var app = (0, _express["default"])();
-var port = process.env.PORT || 5000;
-app.listen(3000);
+_app["default"].listen(3000);
+
 console.log('server listen on port ', 3000);
